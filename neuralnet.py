821d4ce567d132980804435c59f907b3ee9f5191
Mohit Lauer Dubey <MLD@ip-73-46.wireless.oberlin.edu>
2018-10-10 16:56:51 -0400

    fixed neuralnet

--- neuralnet.py
+++ neuralnet.py
@@ -33,11 +33,11 @@ if __name__ == "__main__":
                   [0,1,1],
                   [1,0,1],
                   [1,1,1]])
-                  y = np.array([[0],[1],[1],[0]])
-                  nn = NeuralNetwork(X,y)
+    y = np.array([[0],[1],[1],[0]])
+    nn = NeuralNetwork(X,y)
                   
-                  for i in range(1500):
-                      nn.feedforward()
-                      nn.backprop()
+    for i in range(1500):
+      nn.feedforward()
+      nn.backprop()
 
-print(nn.output)
+    print(nn.output)
